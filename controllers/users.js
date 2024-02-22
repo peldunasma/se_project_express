7d82fdfdec7934031f0b9c998b37f7bda5d4ff1b
Matthew Peldunas <peldunasma@gmail.com>
2024-02-21 21:30:00 -0500

    refactored

--- controllers/users.js
+++ controllers/users.js
@@ -9,7 +9,7 @@ const {
 
 const getUsers = (req, res) => {
   User.find({})
-    .then((users) => res.send(users))
+    .then((users) => res.status(200).send(users))
     .catch((err) => {
       console.error(err);
       return res.status(DEFAULT_ERROR).send({ message: err.message });
@@ -21,7 +21,7 @@ const getUsers = (req, res) => {
 const createUser = (req, res) => {
   const { name, avatar} = req.body;
   User.create({ name, avatar })
-    .then((user) => res.send(user))
+    .then((user) => res.status(201).send(user))
     .catch((err) => {
       console.error(err);
       if (err.name === "ValidationError") {
@@ -37,7 +37,7 @@ const getUser = (req,res) => {
   const { userId } = req.params;
   User.findById(userId)
   .orFail()
-  .then((user) => res.send(user))
+  .then((user) => res.status(200).send(user))
   .catch((err) => {
     console.error(err);
     if (err.name === "DocumentNotFoundError") {
