9c9b6400e0651ca40daa055352cdeed376d8454b
Matthew Peldunas <peldunasma@gmail.com>
2024-02-25 11:49:06 -0500

    refactored

--- controllers/users.js
+++ controllers/users.js
@@ -5,58 +5,54 @@ const {
   DEFAULT_ERROR,
 } = require("../utils/errors");
 
-// GET /users - returns all users
+// returns all users
 
 const getUsers = (req, res) => {
   User.find({})
     .then((users) => res.status(200).send(users))
     .catch((err) => {
       console.error(err);
-      return res
-        .status(DEFAULT_ERROR)
-        .send({ message: "The server has encountered an error" });
+      return res.status(DEFAULT_ERROR).send({ message: err.message });
     });
 };
 
-// POST /users - creates a new user
+// creates a new user
 
 const createUser = (req, res) => {
-  const { name, avatar } = req.body;
+  const { name, avatar} = req.body;
   User.create({ name, avatar })
     .then((user) => res.status(201).send(user))
     .catch((err) => {
       console.error(err);
       if (err.name === "ValidationError") {
-        return res.status(INVALID_DATA_ERROR).send({ message: "Invalid data" });
+      return res.status(INVALID_DATA_ERROR ).send({message: err.message});
       }
-      return res
-        .status(DEFAULT_ERROR)
-        .send({ message: "The server has encountered an error" });
+      return res.status(DEFAULT_ERROR).send({message: err.message});
     });
 };
 
-// GET /users/:userId - returns all users by _Id
+// returns all users by _Id
 
-const getUser = (req, res) => {
+const getUser = (req,res) => {
   const { userId } = req.params;
   User.findById(userId)
-    .orFail()
-    .then((user) => res.status(200).send(user))
-    .catch((err) => {
-      console.error(err);
-      if (err.name === "DocumentNotFoundError") {
-        return res.status(NOTFOUND_ERROR).send({ message: err.message });
-      } else if (err.name === "CastError") {
-        return res.status(NOTFOUND_ERROR).send({ message: "Invalid data" });
-      }
+  .orFail()
+  .then((user) => res.status(200).send(user))
+  .catch((err) => {
+    console.error(err);
+    if (err.name === "DocumentNotFoundError") {
       return res
-        .status(DEFAULT_ERROR)
-        .send({ message: "The server has encountered an error" });
-    });
+          .status(NOTFOUND_ERROR)
+          .send({ message: err.message });
+    } else if (err.name === "CastError") {
+      return res.status(NOTFOUND_ERROR).send({ message: err.message });
+    }
+    return res.status(DEFAULT_ERROR).send({message: err.message});
+  });
 };
 
 module.exports = {
   getUsers,
   createUser,
-  getUser,
-};
+  getUser
+  };
