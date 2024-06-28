d126f491c5ba216552d564dff7b156c96ffd13dd
Matthew Peldunas <peldunasma@gmail.com>
2024-06-28 09:17:26 -0400

    new

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../utils/config");
-const unauthorizedError = require("../utils/errors");
+const unauthorizedError= require("../utils/errors/unauthorizedError");
 
 
 const handleAuthorization = (req, res, next) => {
